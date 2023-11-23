dac136d43d5c4a9896448e20f5cbd87a053b42ad
blazte <blazte.aster@gmail.com>
2023-11-23 12:21:21 -0500

    create tooltip

--- src/features/post/components/post-generator/social-section.tsx
+++ src/features/post/components/post-generator/social-section.tsx
@@ -29,13 +29,21 @@ export const SocialSection = () => {
                         >
                             <Chip
                                 variant={'icon'}
-                                className={` ${
+                                className={`relative group ${
                                     isActive
                                         ? 'bg-green-100 border-green-500 text-green-500'
                                         : ''
                                 }`}
                             >
                                 <Component aria-label={`${name} icon option`} />
+                                <div className="hidden group-hover:flex  absolute left-1/2  top-full px-1 py-2 mt-3 bg-black text-white text-sm w-min rounded-md -translate-x-1/2  justify-center">
+                                    {name}
+                                    <div
+                                        className="absolute mx-auto h-0 w-0 border-r-[5px] border-b-[7px] 
+                                            border-l-[5px] border-solid border-r-transparent
+                                            border-l-transparent border-b-[#000] top-0 mt-[-7px]"
+                                    ></div>
+                                </div>
                             </Chip>
                         </RadioField>
                     )}
