b11d78c1d5f5b57fbddf72e292f076252f6cd010
blazte <blazte.aster@gmail.com>
2023-11-23 17:15:04 -0500

    change function capitalize first letter

--- src/features/post/utils/capitalize-first-letter.ts
+++ src/features/post/utils/capitalize-first-letter.ts
@@ -1,3 +1,6 @@
-export function capitalizeFirstLetter([first, ...rest]) {
-    return first.toUpperCase() + rest.join('')
+export function capitalizeFirstLetter(str: string) {
+    if (typeof str !== 'string' || str.length === 0) {
+        return str
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1)
 }
