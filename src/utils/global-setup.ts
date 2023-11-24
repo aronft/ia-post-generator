e17b16194bc35573103a8c0718c04e3ad6906755
blazte <blazte.aster@gmail.com>
2023-11-24 15:22:19 -0500

    fix config unused

--- src/utils/global-setup.ts
+++ src/utils/global-setup.ts
@@ -1,8 +1,8 @@
-import { FullConfig } from '@playwright/test'
+// import { FullConfig } from '@playwright/test'
 
 import dotenv from 'dotenv'
 
-async function globalSetup(config: FullConfig) {
+async function globalSetup() {
     if (process.env.test_env) {
         dotenv.config({
             path: `.env.${process.env.test_env}`,
